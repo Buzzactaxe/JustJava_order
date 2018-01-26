4c4db72ae0e2994a0febc5e8b04ec6a3e64bb8b2
Jesters_Race <valiantsimpson@gmail.com>
2018-01-26 15:15:59 +0100

    Modified MainActivity and added new File 'TestAddFile.java'

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -137,4 +137,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //all about the changes
+
 }
+
---
+++ app/src/main/java/com/example/android/justjava/TestAddFile.java
@@ -0,0 +1,8 @@
+package com.example.android.justjava;
+
+/**
+ * Created by valia on 26/01/2018.
+ */
+
+public class TestAddFile {
+}
